5e59d9eb160cc697dc0dea2dd190cb6b3d7fc2af
Aboodkhazaleh <aboodkhazaleh4@gmail.com>
2026-05-13 16:52:06 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDJA4tYSKiuemRx5QR-9iq1HJ9kdUOHtxY",
+  authDomain: "waqar-6364f.firebaseapp.com",
+  projectId: "waqar-6364f",
+  storageBucket: "waqar-6364f.firebasestorage.app",
+  messagingSenderId: "339253062131",
+  appId: "1:339253062131:web:a0f1934763c3604a278a86",
+  measurementId: "G-X81VXS7MZD"
+};
